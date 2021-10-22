0ba039c3ffd88fa60b5ef74000adb7003c7efb5c
Jose Gamboa <joalgamro2010@gmail.com>
2021-10-22 14:02:24 -0600

    A: sharedMemory

---
+++ headers/sharedMemory.h
@@ -0,0 +1,15 @@
+#ifndef SHARED_MEMORY_H
+#define SHARED_MEMORY_H
+
+#include <stdbool.h>
+
+static int createSharedBlock(char *filename, int pos, int size);
+static int getSharedBlock(char *filename, int pos);
+bool destroyMemoryBlock(char *filename, int pos);
+void * attachMemoryBlock(char *filename, int pos);
+bool detachMemoryBlock(void *block);
+
+#define FILENAME "shmBlock"
+#define IPC_RESULT_ERROR (-1)
+
+#endif
